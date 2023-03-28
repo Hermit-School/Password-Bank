fa48dc62b547c91fc83ebc2667e979259d7f0f97
rashmishanbhogue <rashmi.shanbhogue@gmail.com>
2023-03-28 22:20:16 +0530

    login_page, password_data
    need a look over at the validation logic for email and password textfields in login_page
    cleared password_data file

--- passwordbank/lib/login_page.dart
+++ passwordbank/lib/login_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:resume_view/forgot_password.dart';
 import 'auth_screen.dart';
-import 'password_data.dart';
+// import 'password_data.dart';
 import 'forgot_password.dart';
-//import 'login_page.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -15,6 +14,7 @@ class LoginPageState extends State<LoginPage> {
   String password = 'enter password here';
   Color buttonColor = Colors.white;
   TextEditingController controller = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -46,130 +46,183 @@ class LoginPageState extends State<LoginPage> {
               color: Colors.white,
             ),
           ),
-          body: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('Welcome back!',
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Montserrat',
-                        color: Colors.white,
-                      )),
-                  SingleChildScrollView(
-                      physics: AlwaysScrollableScrollPhysics(),
-                      child: SizedBox(height: 50)),
-                  TextField(
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xFFFACB1B))),
-                      labelText: "Email",
-                      labelStyle: TextStyle(color: Colors.white),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFF818284)),
-                      ),
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        email = value;
-                      });
-                    },
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  TextField(
-                    style: TextStyle(color: Colors.white),
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xFFFACB1B))),
-                      labelText: "Password",
-                      labelStyle: TextStyle(color: Colors.white),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFF818284)),
-                      ),
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        password = value;
-                      });
-                    },
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  // InkWell(
-                  //     // child: Container(
-                  //     //     color: Color(0xFFFACB1B),
-                  //     //     width: 150,
-                  //     //     height: 30,
-                  //     child: Center(
-                  //         child: Text('Forgot Password?',
-                  //             style: TextStyle(
-                  //               color: Color(0xFF692E19),
-                  //             ))),
-                  //     // ),
-                  //     onTap: () {
-                  //       // setState(() {
-                  //       ForgotPasswordPage();
-                  //     }),
-                  // // },
-                  TextButton(
-                    style: TextButton.styleFrom(
-                        textStyle: const TextStyle(
-                            fontSize: 20,
-                            fontFamily: 'Montserrat',
-                            decoration: TextDecoration.underline),
-                        foregroundColor: Color(0xFF8F4024)),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ForgotPasswordPage()),
-                      );
-                    },
-                    child: const Text(
-                      'Forgot Password?',
-                      selectionColor: Colors.white,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //       builder: (context) => PasswordHomePage()),
-                      // );
-                    },
-                    style: ButtonStyle(
-                      foregroundColor:
-                          MaterialStateProperty.all(Color(0xFF32BEA6)),
-                      backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                        (states) {
-                          if (states.contains(MaterialState.pressed)) {
-                            return Color(
-                                0xFFFACB1B); // The button color when it's pressed
-                          } else {
-                            return buttonColor; // The button color when it's not pressed
-                          }
-                        },
-                      ),
-                    ),
-                    child: Text("Submit",
+          body: Form(
+            key: formKey,
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Welcome back!',
                         style: TextStyle(
-                          fontSize: 20.0,
+                          fontSize: 30.0,
                           fontWeight: FontWeight.bold,
                           fontFamily: 'Montserrat',
+                          color: Colors.white,
                         )),
-                  ),
-                ]),
+                    SingleChildScrollView(
+                        physics: AlwaysScrollableScrollPhysics(),
+                        child: SizedBox(height: 50)),
+                    TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFFACB1B))),
+                          labelText: "Email",
+                          labelStyle: TextStyle(color: Colors.white),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF818284)),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            email = value;
+                          });
+                          validator:
+                          (value) {
+                            if (value.isEmpty) {
+                              return 'E-mail cannot be empty!';
+                            } else if (!RegExp(
+                                    r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                .hasMatch(value)) {
+                              return 'Invalid e-mail address';
+                            }
+                            return null;
+                          };
+                        }),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    TextField(
+                        style: TextStyle(color: Colors.white),
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFFACB1B))),
+                          labelText: "Password",
+                          labelStyle: TextStyle(color: Colors.white),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF818284)),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            password = value;
+                          });
+                          validator:
+                          (value) {
+                            if (value.isEmpty) {
+                              return 'Password cannot be empty';
+                            } else if (value.length < 8) {
+                              return 'Password must be at least 8 characters long';
+                            }
+                            return null;
+                          };
+                        }),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    // InkWell(
+                    //     // child: Container(
+                    //     //     color: Color(0xFFFACB1B),
+                    //     //     width: 150,
+                    //     //     height: 30,
+                    //     child: Center(
+                    //         child: Text('Forgot Password?',
+                    //             style: TextStyle(
+                    //               color: Color(0xFF692E19),
+                    //             ))),
+                    //     // ),
+                    //     onTap: () {
+                    //       // setState(() {
+                    //       ForgotPasswordPage();
+                    //     }),
+                    // // },
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          textStyle: const TextStyle(
+                              fontSize: 18,
+                              fontFamily: 'Montserrat',
+                              decoration: TextDecoration.underline),
+                          foregroundColor: Color(0xFF8F4024)),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ForgotPasswordPage()),
+                        );
+                      },
+                      child: const Text(
+                        'Forgot Password?',
+                        selectionColor: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (formKey.currentState != null &&
+                            formKey.currentState!.validate()) {
+                          SizedBox(
+                            height: 30.0,
+                          );
+                          Text(
+                            'Processing Data',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'Montserrat',
+                                color: Colors.white),
+                          );
+                          // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          //   content: Text('Processing Data'),
+                          //   duration: Duration(seconds: 1),
+                          //   action: SnackBarAction(
+                          //     label: 'Enter!',
+                          //     onPressed: () {},
+                          //   ),
+                          // ));
+                        } else if (formKey.currentState == null ||
+                            formKey.toString().isEmpty) {
+                          Text(
+                            'Email can\'t be empty',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'Montserrat',
+                                color: Colors.white),
+                          );
+                        }
+
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //       builder: (context) => PasswordHomePage()),
+                        // );
+                      },
+                      style: ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all(Color(0xFF32BEA6)),
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith<Color>(
+                          (states) {
+                            if (states.contains(MaterialState.pressed)) {
+                              return Color(
+                                  0xFFFACB1B); // The button color when it's pressed
+                            } else {
+                              return buttonColor; // The button color when it's not pressed
+                            }
+                          },
+                        ),
+                      ),
+                      child: Text("Sign-In",
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat',
+                          )),
+                    ),
+                  ]),
+            ),
           ),
         ));
   }
